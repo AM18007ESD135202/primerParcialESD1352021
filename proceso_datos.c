d22880b8f79b0297fd03a5a3b8061b99549d9740
avi <am18007@ues.edu.sv>
2021-09-05 18:01:30 -0600

    completo 2.3

--- proceso_datos.c
+++ proceso_datos.c
@@ -141,5 +141,5 @@ tipo_dato** multliplicacion_matriz(tipo_dato **V_1,int fila,int columas,tipo_dat
 
 
 const char* carnet(){
-    return "Aviles Morán Ernesto Alexander,AM18007";
+    return "AM18007";
 }
